759c13185d471a36e01ba19003cea73a6e4928ec
cristian bravo <cristian.bravo@catena.cl>
2018-05-08 00:51:45 -0300

    commit inicial

---
+++ src/app/components/template-driven/template-driven.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-template-driven',
+  templateUrl: './template-driven.component.html',
+  styleUrls: ['./template-driven.component.css']
+})
+export class TemplateDrivenComponent implements OnInit {
+  emailId:string;
+
+  constructor() {
+    this.emailId = '';
+  }
+
+  ngOnInit() {
+  }
+
+  onClickSubmit(data) {
+    this.emailId = data.emailid;
+  }
+}
