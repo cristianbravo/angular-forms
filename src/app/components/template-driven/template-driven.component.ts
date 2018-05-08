88ada678db8d31a5736f41727c4cd01bf39ad026
cristian bravo <cristian.bravo@catena.cl>
2018-05-08 01:41:54 -0300

    patterns en validators

--- src/app/components/template-driven/template-driven.component.ts
+++ src/app/components/template-driven/template-driven.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateDrivenComponent implements OnInit {
   emailId:string;
+  emailPattern:string;
 
   constructor() {
     this.emailId = '';
+    this.emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$';
   }
 
   ngOnInit() {
